30b8d33c6c25922f47d5f5d1391728be5e4a8ea7
fractal2k <e27sam@gmail.com>
2019-10-08 23:14:58 +0530

    I'm a shameless piece of shit who plagiarises code (Notifications added)

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,10 +1,12 @@
 const functions = require("firebase-functions");
 const app = require("express")(); // Initialize express app
+const { db } = require("./util/init");
 const {
   signup,
   login,
   uploadImage,
-  addUserDetails
+  addUserDetails,
+  getAuthenticatedUser
 } = require("./route_handlers/users"); // Import user route handlers
 const {
   createEvent,
@@ -25,6 +27,7 @@ app.post("/signup", signup); // Signup to the website
 app.post("/login", login); // Login to the website
 app.post("/user/image", auth, uploadImage); // Upload profile image
 app.post("/user", auth, addUserDetails); // Add all the other user details
+app.get("/user/profile", auth, getAuthenticatedUser);
 
 // Event generation/management routes
 app.post("/event/create", auth, fileParser, checkAdmin, createEvent); // Create an event
@@ -37,3 +40,39 @@ app.get("/event/:eventID/like", auth, likeEvent); // Like an event
 app.get("/event/:eventID/unlike", auth, unlikeEvent); // Unlike an event
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document("likes/{id}")
+  .onCreate(snapshot => {
+    return db
+      .doc(`/events/${snapshot.data().eventID}`)
+      .get()
+      .then(doc => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            eventID: doc.id
+          });
+        }
+      })
+      .catch(err => console.error(err));
+  });
+
+exports.deleteNotificationOnUnLike = functions.firestore
+  .document("likes/{id}")
+  .onDelete(snapshot => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch(err => {
+        console.error(err);
+        return;
+      });
+  });
--- src/functions/route_handlers/users.js
+++ src/functions/route_handlers/users.js
@@ -178,3 +178,50 @@ exports.uploadImage = (req, res) => {
   });
   busboy.end(req.rawBody);
 };
+
+// Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection("likes")
+          .where("userHandle", "==", req.user.handle)
+          .get();
+      }
+    })
+    .then(data => {
+      userData.likes = [];
+      data.forEach(doc => {
+        userData.likes.push(doc.data());
+      });
+      return db
+        .collection("notifications")
+        .where("recipient", "==", req.user.handle)
+        .orderBy("createdAt", "desc")
+        .limit(10)
+        .get();
+    })
+    .then(data => {
+      userData.notifications = [];
+      data.forEach(doc => {
+        userData.notifications.push({
+          recipient: doc.data().recipient,
+          sender: doc.data().sender,
+          createdAt: doc.data().createdAt,
+          screamId: doc.data().screamId,
+          type: doc.data().type,
+          read: doc.data().read,
+          notificationId: doc.id
+        });
+      });
+      return res.json(userData);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
