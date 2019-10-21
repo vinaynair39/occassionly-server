b0f099d5947d93c7c156fda81e8207696ff2e315
vinay <vnnair39@gmail.com>
2019-10-21 12:05:43 +0530

    I guess I fixed something but dont remember what

--- src/functions/route_handlers/users.js
+++ src/functions/route_handlers/users.js
@@ -239,6 +239,23 @@ exports.getUserHandle = (req,res) => {
     return res.send(req.user.handle);
 }
 
+exports.markNotificationsRead = (req, res) => {
+    let batch = db.batch();
+    req.body.forEach((notificationId) => {
+      const notification = db.doc(`/notifications/${notificationId}`);
+      batch.update(notification, { read: true });
+    });
+    batch
+      .commit()
+      .then(() => {
+        return res.json({ message: 'Notifications marked read' });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  };
+
 
 exports.followUser = (req, res) => {
   let batch = db.batch();
